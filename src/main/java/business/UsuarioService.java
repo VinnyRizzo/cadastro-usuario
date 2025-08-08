040c880b25b7a0977627b7a32bf4bef0d4a8ec51
VinnyRizzo <viniciusbragarizzo@gmail.com>
2025-08-08 18:26:22 -0300

    Buscar Usuário

--- src/main/java/business/UsuarioService.java
+++ src/main/java/business/UsuarioService.java
@@ -15,5 +15,10 @@ public class UsuarioService {
 	public void salvarUsuario(Usuario usuario) {
 		repository.saveAndFlush(usuario);
 	}
+	
+	public Usuario buscarUsuarioPorEmail(String email) {
+		return repository.findByEmail(email).orElseThrow(
+				() -> new RuntimeException("Email não encontrado"));
+	}
 
 }
